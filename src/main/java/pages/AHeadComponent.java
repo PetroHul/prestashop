9318e2fda11abc9b02da32764d01ddc3776a6a78
Iryna Hrytsai <irynahrytsai@gmail.com>
2018-12-08 18:52:25 +0200

    Add search elements to AHeadComponent

--- src/main/java/pages/AHeadComponent.java
+++ src/main/java/pages/AHeadComponent.java
@@ -1,11 +1,38 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class AHeadComponent  {
     protected WebDriver driver;
 
+//    need for search, will change to enum
+    protected final String SEARCH_VALUE= "mug";
+
     protected AHeadComponent(WebDriver driver){
         this.driver = driver;
+
     }
+
+//    search
+    public WebElement getSearchProductField(){
+        return driver.findElement(By.name("s"));
+    }
+    public String getSearchProductFieldText(){
+        return getSearchProductField().getAttribute(SEARCH_VALUE);
+    }
+    public void setSearchProductField(String text){
+        getSearchProductField().sendKeys(text);
+    }
+    public void clearSearchProductField(){
+        getSearchProductField().clear();
+    }
+    public WebElement getSearchProductButton(){
+        return driver.findElement(By.cssSelector("button[type='submit']"));
+    }
+    public void clickSearchProductButton(){
+        getSearchProductButton().click();
+    }
+
 }
