7a7c1ba4f5cac4146523ee617bdb54fe9e16b74f
Liubomyr Podoliak <L.podoliak@gmail.com>
2018-12-09 20:43:59 +0200

    rename my test

--- src/test/java/CartFunctionalTest.java
+++ src/test/java/CartFunctionalTest.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.*;
@@ -9,9 +11,9 @@ import tools.TestRunner;
 import java.util.concurrent.TimeUnit;
 
 
-public class AddProductToCart extends TestRunner {
+public class CartFunctionalTest extends TestRunner {
     @Test
-    public void test1() throws InterruptedException {
+    public void addProductToShoppingCart() {
         //arrange
         HomePage homePage = loadAplication();
         delayExecution(1000);
@@ -19,13 +21,13 @@ public class AddProductToCart extends TestRunner {
         homePage.getProductsListComponent().getProductComponentByPartialName("Hummingbird Printed T-Shirt").clickToProduct();
         ProductPage productPage = new ProductPage(driver);
         productPage.clickToAddButton();
-        delayExecution(1000);
+
+        delayExecution(10000);
 
         ProductMessagePage productMessagePage = new ProductMessagePage(driver);
         productMessagePage.closeAlertMessage();
 
         delayExecution(10000);
-
         productMessagePage.clickShoppingCart();
         ShoppingCartPage soppingCartPage = new ShoppingCartPage(driver);
         //assert
