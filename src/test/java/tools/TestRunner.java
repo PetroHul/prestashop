ce3d264ebff8ea65e7c1e0bb34187927cda5a8e0
Liubomyr Podoliak <L.podoliak@gmail.com>
2018-12-12 22:51:50 +0200

    add change in CartTest and Change Dependency

--- src/test/java/tools/TestRunner.java
+++ src/test/java/tools/TestRunner.java
@@ -2,7 +2,10 @@ package tools;
 
 import data.IUser;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 import pages.HomePage;
 import pages.LoginPage;
@@ -23,8 +26,6 @@ public abstract class TestRunner {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.get("http://studio5f.online/en/");
-
     }
 
     @AfterClass(alwaysRun = true)
@@ -32,10 +33,10 @@ public abstract class TestRunner {
         driver.quit();
     }
 
-//    @BeforeMethod
-//    public void beforeMethod() {
-//        driver.get("http://localhost/PRESTASHOP/en/index.php");
-//    }
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.get("http://studio5f.online/en/");
+    }
 
     protected HomePage loadAplication() {
         return new HomePage(driver);
@@ -45,6 +46,13 @@ public abstract class TestRunner {
         HomePage homePage = new HomePage(driver);
         homePage.clickSignInButton().logInAcount(user).clickLogo();
         return homePage;
+
+    }
+
+    protected void delayExplicitExecution(WebElement webElement){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        wait.until(ExpectedConditions.elementToBeClickable(webElement));
 
     }
 
