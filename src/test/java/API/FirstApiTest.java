fc355aac24b04d9e131026e2de66e600c59a5729
PetroHul <barzoom1@gmail.com>
2018-12-12 15:35:59 +0200

    api-first

---
+++ src/test/java/API/FirstApiTest.java
@@ -0,0 +1,32 @@
+package API;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+import tools.TestRunner;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import org.testng.annotations.Test;
+//import com.jayway.restassured.RestAssured;
+import static org.testng.Assert.assertTrue;
+public class FirstApiTest {
+
+    @Test
+    public void testFirstAPI() throws InterruptedException{
+        given().
+                get("http://Z59BV19EUGEUMFAFWRL2DBRGMF4114MF@studio5f.online/api/products/2").
+         then().
+                body(RestResponse.result.name)
+    }
+
+
+//    @Before
+//    public void setup() {
+//        RestAssured.baseURI = "https://api.github.com";
+//        RestAssured.port = 443;
+//    }
+//
+}
