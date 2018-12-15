865a9f7ee328299957638bae02a81320297f4aa1
VitaliiKrasov <krasovvitalii@gmail.com>
2018-12-15 16:23:46 +0200

    wtf

--- src/test/java/tools/TestRunner.java
+++ src/test/java/tools/TestRunner.java
@@ -23,7 +23,7 @@ public abstract class TestRunner {
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @BeforeMethod
