72d08dac247dc2a70d6fc81478a7b81c159446e2
PetroHul <barzoom1@gmail.com>
2018-12-16 19:32:58 +0200

    good-test-petro

--- src/test/java/IdentityAccountTest.java
+++ src/test/java/IdentityAccountTest.java
@@ -16,8 +16,8 @@ public class IdentityAccountTest extends TestRunner{
         //
 
         @DataProvider(name = "newuser")
-        public static Object[][] insertUser() throws SQLException{
-            String insert = "INSERT INTO `ps_customer` (`id_customer`, `id_shop_group`, `id_shop`, `id_gender`," +
+        public static Object[][] insertUser() {
+            final String insert = "INSERT INTO `ps_customer` (`id_customer`, `id_shop_group`, `id_shop`, `id_gender`," +
                     " `id_default_group`, `id_lang`, `id_risk`, `company`, `siret`," +
                     " `ape`, `firstname`, `lastname`, `email`, `passwd`," +
                     " `last_passwd_gen`, `birthday`, `newsletter`," +
@@ -27,17 +27,14 @@ public class IdentityAccountTest extends TestRunner{
                     " `note`, `active`, `is_guest`, `deleted`, `date_add`," +
                     " `date_upd`, `reset_password_token`, `reset_password_validity`)" +
                     " VALUES (NULL, '1', '1', '0', '3', '2', '0', '', '', '', 'Petro'," +
-                    " 'Hu" +
-                    "" +
-                    "" +
-                    "l', 'setstore@set.ua'," +
+                    " 'Hu" + "" + "" + "l', 'setstore@set.ua'," +
                     " '$2y$10$Gr87PNmdP0Fntv9IXn5iy.LEOcl4KGF0qZMWuYxqpNfNFXcg28pAy'," +
                     " '2018-12-15 14:11:20', '0000-00-00', '0', '', '0000-00-00 00:00:00'," +
                     " '0', '', '0.000000', '0', '0', 'fa0aaaf5a5410dd59b2b4cfe9dddaf2c'," +
                     " '', '1', '0', '0', '2018-12-15 20:11:20', '2018-12-15 20:11:20'," +
                     " '', '0000-00-00 00:00:00')";
 
-            String insert1 ="INSERT INTO `ps_customer` (`id_customer`, `id_shop_group`," +
+            final String insert1 ="INSERT INTO `ps_customer` (`id_customer`, `id_shop_group`," +
                     " `id_shop`, `id_gender`, `id_default_group`, `id_lang`, `id_risk`," +
                     " `company`, `siret`, `ape`, `firstname`, `lastname`, `email`, `passwd`," +
                     " `last_passwd_gen`, `birthday`, `newsletter`, `ip_registration_newsletter`," +
@@ -58,74 +55,49 @@ public class IdentityAccountTest extends TestRunner{
             final String password1 = "asdfew21a";
             final String newPassword1 = "147852";
 
-            String delete = "DELETE FROM ps_customer WHERE email="+"\"setstore@set.ua\";";
-            String delete1 = "DELETE FROM ps_customer WHERE email="+"\"superstor@gmail.set\";";
+            final String delete = "DELETE FROM ps_customer WHERE email="+"\"setstore@set.ua\";";
+            final String delete1 = "DELETE FROM ps_customer WHERE email="+"\"superstor@gmail.set\";";
 
-            return new Object[][] {{ insert, email, password, newPassword, delete}, { insert1, email1, password1, newPassword1, delete1 }};
+            return new Object[][] {{ insert, email, password, newPassword, delete},
+                    { insert1, email1, password1, newPassword1, delete1 }};
         }
-
-//    @BeforeMethod()
-//    public void init() throws SQLException {
-//        ConncectDB conncectDB = new ConncectDB();
-//
-//        String insert = "INSERT INTO `ps_customer` (`id_customer`, `id_shop_group`, `id_shop`, `id_gender`," +
-//                " `id_default_group`, `id_lang`, `id_risk`, `company`, `siret`," +
-//                " `ape`, `firstname`, `lastname`, `email`, `passwd`," +
-//                " `last_passwd_gen`, `birthday`, `newsletter`," +
-//                " `ip_registration_newsletter`, `newsletter_date_add`," +
-//                " `optin`, `website`, `outstanding_allow_amount`," +
-//                " `show_public_prices`, `max_payment_days`, `secure_key`," +
-//                " `note`, `active`, `is_guest`, `deleted`, `date_add`," +
-//                " `date_upd`, `reset_password_token`, `reset_password_validity`)" +
-//                " VALUES (NULL, '1', '1', '0', '3', '2', '0', '', '', '', 'Petro'," +
-//                " 'Hul', 'setstore@set.ua'," +
-//                " '$2y$10$Gr87PNmdP0Fntv9IXn5iy.LEOcl4KGF0qZMWuYxqpNfNFXcg28pAy'," +
-//                " '2018-12-15 14:11:20', '0000-00-00', '0', '', '0000-00-00 00:00:00'," +
-//                " '0', '', '0.000000', '0', '0', 'fa0aaaf5a5410dd59b2b4cfe9dddaf2c'," +
-//                " '', '1', '0', '0', '2018-12-15 20:11:20', '2018-12-15 20:11:20'," +
-//                " '', '0000-00-00 00:00:00')";
-//
-////        String insert2 = "INSERT INTO `ps_customer_group` (`id_customer`, `id_group`) VALUES ('76', '3')";
-//        conncectDB.DMLDataQuery(insert);
-//    }
 
     @Test(dataProvider = "newuser")
     public void changePasswordTest(String insert, String email, String password, String newPassword, String delete) throws SQLException {
         ConncectDB conncectDB = new ConncectDB();
         conncectDB.DMLDataQuery(insert);
-//        final String email = "setstore@set.ua";
-//        final String password = "529440";
-//        final String newPassword = "268405";
 
-        //load app
         HomePage homePage = loadApplication();
         homePage.clickSignInButton();
-        //logining account
+
         LoginPage loginPage = new LoginPage(driver);
         loginPage.signIn(email,password);
-        //go to information
+
         MyAccountPage myAccount = new MyAccountPage(driver);
         myAccount.clickInformation();
-        //change password
+
         IdentityAccountPage identity = new IdentityAccountPage(driver);
         identity.typePassword(password);
         identity.typeNewPassword(newPassword);
         identity.clickButtonSave();
+
         //check massage
         String actual = identity.getAlertSuccessText();
         String expected = "Information successfully updated.";
         assertEquals(actual,expected);
+
         //logout
         identity.clickSignOutButtom();
+
         //login
         LoginPage loginPage1 = new LoginPage(driver);
         loginPage1.signIn(email,newPassword);
+
         //get actual url
         String actual1 = driver.getCurrentUrl();
         boolean expected1 = actual1.contains("identity");
         assertTrue(expected1);
-        //del user and group for user
-        //ConncectDB conncectDB = new ConncectDB();
-            conncectDB.DMLDataQuery(delete);// make to work
+
+        conncectDB.DMLDataQuery(delete);
     }
 }
