aeac991f163386c28113ba6e6350da2848dbbcf4
Nataliia <khodak.natasha@gmail.com>
2018-12-16 08:54:00 +0200

    currencyFunctionalTest

--- src/test/java/CurrencyFunctionalTest.java
+++ src/test/java/CurrencyFunctionalTest.java
@@ -1,17 +1,16 @@
 import data.Category;
 import data.Currencies;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import pages.AHeadComponent;
-import pages.CategoryPage;
-import pages.HomePage;
+import pages.*;
 import tools.TestRunner;
 
 public class CurrencyFunctionalTest extends TestRunner  {
 
-    private HomePage homePage;
-    @Test
-    public void checkCurrencyTest() {
+//    private HomePage homePage;
+//    @Test
+//    public void checkCurrencyTest() {
 //        CategoryPage accessoriesPage;
 //        String actual;
 //        String expected = Category.ACCESSORIES.toString();
@@ -22,12 +21,33 @@ public class CurrencyFunctionalTest extends TestRunner  {
 //        Assert.assertEquals(actual, expected);
 //    }
 
-        AHeadComponent currencyPage;
-        String actual;
-        String expected = Currencies.UAH.toString();
+//        AHeadComponent currencyPage;
+//        String actual;
+//        String expected = Currencies.UAH.toString();
 //act
 //        currencyPage = homePage.clickCurrency();
 
+
+//    }
+
+
+
+    @Test
+    public void addToCartTest() {
+
+        ShoppingCartPage shoppingCartPage = new ShoppingCartPage(driver);
+
+        Assert.assertTrue(shoppingCartPage.getProductNameText().equals("Hummingbird printed t-shirt"));
+    }
+
+    @Test
+    public void deleteProductFromShoppingCartTest() {
+
+        ShoppingCartPage shoppingCartPage = new ShoppingCartPage(driver);
+        shoppingCartPage.clickDeleteButton();
+        ShoppingEmptyCartPage shoppingEmptyCartPage = new ShoppingEmptyCartPage(driver);
+
+        Assert.assertTrue(shoppingEmptyCartPage.getMessageText().equals("There are no more items in your cart"));
 
     }
 }
