6a502840a569e8be6e8e802890f1a02c7b78ccc1
PetroHul <barzoom1@gmail.com>
2018-12-23 00:55:40 +0200

    for ubuntu

--- src/test/java/tools/TestRunner.java
+++ src/test/java/tools/TestRunner.java
@@ -23,7 +23,8 @@ public abstract class TestRunner {
 
     @BeforeClass
     public void setUp() {
-        String property = System.getProperty("user.dir") + "/driver/chromedriver";
+//        String property = System.getProperty("user.dir") + "/driver/chromedriver";
+        String property = System.getProperty("user.dir") + "/usr/bin/chromedriver";
         System.setProperty("webdriver.chrome.driver", property);
         driver = new ChromeDriver();
 //        driver = new FirefoxDriver();
