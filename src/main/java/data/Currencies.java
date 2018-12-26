50f20f95f54ac62b3f3679fd4fad978c17096ac5
Vitalii <krasovvitalii@gmail.com>
2018-12-26 13:09:32 +0200

    Update Currencies.java

--- src/main/java/data/Currencies.java
+++ src/main/java/data/Currencies.java
@@ -1,20 +1,20 @@
 package data;
 
 public enum Currencies {
-    EUR,//("EUR"),
-    UAH,//("UAH"),
-    USD;//("USD");
-    //
-//    private String name;
-//
-//    private Currencies(String name) {
-//        this.name = name;
-//    }
+    EUR("EUR"),
+    UAH("UAH"),
+    USD("USD");
+    
+    private String name;
 
-//    @Override
-//    public String toString() {
-//        return name;
-//    }
+    private Currencies(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 
 }
 
