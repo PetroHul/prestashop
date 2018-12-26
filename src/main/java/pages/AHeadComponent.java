cf03b79cd7c38191429f05a400c70472f3803c66
Vitalii <krasovvitalii@gmail.com>
2018-12-26 13:13:54 +0200

    Update AHeadComponent.java

--- src/main/java/pages/AHeadComponent.java
+++ src/main/java/pages/AHeadComponent.java
@@ -69,17 +69,8 @@ public abstract class AHeadComponent {
             initListOptions(searchLocator);
         }
 
-//        public DropdownOptions(By searchLocator, By lastLocator) {
-//            initListOptions(searchLocator);
-//            listOptions.add(driver.findElement(lastLocator));
-//            //listOptions.add(search.getWebElement(lastLocator));
-//        }
-
         private void initListOptions(By searchLocator){
             listOptions = driver.findElements(searchLocator);
-            //listOptions = search.getWebElements(searchLocator);
-            //listOptions = driver.findElements(By.cssSelector(".list-inline > li > a.dropdown-toggle + ul > li > a"));
-            //listOptions = myAccount.findElements(By.cssSelector("+ ul > li > a"));
         }
 
         public List<WebElement> getListOptions() {
